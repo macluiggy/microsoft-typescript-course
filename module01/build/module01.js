f400b1c19c93cc5d5562a066805a923ad55ae122
macluiggy <ferrinluiggy@gmail.com>
2021-11-01 17:42:37 -0500

    learning about declaring variables in typescript

--- module01/build/module01.js
+++ module01/build/module01.js
@@ -2,4 +2,14 @@
 function addNumbers(x, y) {
     return x + y;
 }
-console.log(addNumbers(3, 6));
+console.log(addNumbers(4, 6));
+let a;
+function hello() {
+    let a;
+    return a;
+}
+let x;
+let y = 1;
+x = 1;
+y = 2;
+let z;
--- module01/module01.ts
+++ module01/module01.ts
@@ -1,7 +1,7 @@
 function addNumbers(x: number, y: number) {
   return x + y;
 }
-console.log(addNumbers(3, 6));
+console.log(addNumbers(4, 6));
 
 let a
 
@@ -9,3 +9,12 @@ function hello () {
   let a;
   return a
 }
+
+let x: number;
+let y = 1;
+x = 1; y = 3
+let z: any;
+z = 2
+z = 'hello'
+
+let e: undefined = undefined
