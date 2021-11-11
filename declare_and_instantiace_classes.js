749da726051cb6ecdf1d91444dfffb40e1d1ddd5
macluiggy <ferrinluiggy@gmail.com>
2021-11-11 09:50:05 -0500

    using static in classes

--- declare_and_instantiace_classes.js
+++ declare_and_instantiace_classes.js
@@ -4,7 +4,12 @@ var Car = /** @class */ (function () {
         if (doors === void 0) { doors = 4; }
         this._make = make;
         this._color = color;
-        this._doors = doors;
+        if (doors % 2 === 0) {
+            this._doors = doors;
+        }
+        else {
+            throw new Error("Doors must be a multiple of 2");
+        }
     }
     Object.defineProperty(Car.prototype, "make", {
         // Accessors
@@ -49,11 +54,25 @@ var Car = /** @class */ (function () {
     Car.prototype.brake = function () {
         return this.worker() + " is braking with the standard braking system";
     };
+    Car.prototype.turn = function (direction) {
+        return this.worker() + " is turning " + direction;
+    };
     Car.prototype.worker = function () {
         return this._make;
     };
     return Car;
 }());
 var myCar1 = new Car('Cool Car Company', 'blue', 2);
-//console.log(myCar1.color);//devuelve el color del carro
-console.log(myCar1._color);
+//console.log(myCar1.color);//este en cambio devuelve el valor que retorna el getter color
+// se podria usar/utilizar getColor como nombre del getter para evitar confuciones
+//console.log(myCar1._color);//devuelve el color del carro
+var myCar2 = new Car('Galaxy Motors', 'red', 3 + 1);
+//console.log(myCar2.color);//este en cambio devuelve el valor que retorna el getter color
+// se podria usar/utilizar getColor como nombre del getter para evitar confuciones
+//console.log(myCar2._color);//devuelve el color del carro, este caso no se muestra el error ya que se tiene que declarar un validacion en el construtor
+//myCar2.doors = 5 //en este caso si se muestra el error, ya que aqui se llama a el setter, que es el que contiene la validacion, para que la validacion se haga desde el principio se debe hacer en el construtor
+var myCar3 = new Car('Galaxy motors', 'gray');
+console.log(myCar3.doors); //este retorna 4 al ser el valor por defecto
+console.log(myCar3.accelerate(35));
+console.log(myCar3.brake());
+console.log(myCar3.turn('right'));
--- declare_and_instantiace_classes.ts
+++ declare_and_instantiace_classes.ts
@@ -1,8 +1,9 @@
 class Car {
-    //Properties, esta se usa para especificar el typo de la propiedad, se usa _ para diferenciarlos de los parametros que se usan en el construcor, aunque no son necesarios
-    _make: string;
-    _color: string;
-    _doors: number;
+    //Properties, esta se usa para especificar el typo de la propiedad, se usa _ para diferenciarlos de los parametros que se usan en el construcor, aunque no son necesarios y puede tener el mismo nombre que los parametros
+    private static numberOfCars: number = 0;
+    private _make: string;
+    private _color: string;
+    private _doors: number;
     // Constructor, aqui se colocan los parametros que provienen de los argumentos al instanciar la clase
     constructor(make: string, color: string, doors = 4 ) {
         this._make = make;
@@ -12,6 +13,7 @@ class Car {
         } else {
             throw new Error("Doors must be a multiple of 2")
         }
+        Car.numberOfCars++ //cada vez que se "construya" (instantiace) un nuevo carro, aumenta el numeros de carros a 1
     }
     // Accessors
     get make() {
@@ -46,12 +48,18 @@ class Car {
     turn(direction: 'left' | 'right'): string {
         return `${this.worker()} is turning ${direction}`;
     }
-    worker(): string {
+    private worker(): string {
         return this._make
     }
+    // static
+    public static getNumberOfCars(): number {
+        return Car.numberOfCars;
+    }
 }
+console.log(Car.getNumberOfCars());//dara 0 ya que aun no se ha instanciado ningun carro
 
 let myCar1 = new Car('Cool Car Company', 'blue', 2)
+// myCar1.worker() //esto dara error ya que este solo es accecible dentro de la clase
 //console.log(myCar1.color);//este en cambio devuelve el valor que retorna el getter color
 // se podria usar/utilizar getColor como nombre del getter para evitar confuciones
 //console.log(myCar1._color);//devuelve el color del carro
@@ -63,7 +71,9 @@ let myCar2 = new Car('Galaxy Motors', 'red', 3+1);
 //myCar2.doors = 5 //en este caso si se muestra el error, ya que aqui se llama a el setter, que es el que contiene la validacion, para que la validacion se haga desde el principio se debe hacer en el construtor
 
 let myCar3 = new Car('Galaxy motors', 'gray')
-console.log(myCar3.doors); //este retorna 4 al ser el valor por defecto
-console.log(myCar3.accelerate(35));
-console.log(myCar3.brake());
-console.log(myCar3.turn('right'));
+// console.log(myCar3.doors); //este retorna 4 al ser el valor por defecto
+// console.log(myCar3.accelerate(35));
+// console.log(myCar3.brake());
+// console.log(myCar3.turn('right'));
+//console.log(myCar3.worker());
+console.log(Car.getNumberOfCars()); //dara 3 ya que hasta esta linea de codigo ya se han instanciado 3 carros
