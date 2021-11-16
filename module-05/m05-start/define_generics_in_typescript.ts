da381e07b52e7a4054b2005a90c0aceb5fdadb16
macluiggy <ferrinluiggy@gmail.com>
2021-11-16 10:47:57 -0500

    using generics in interfaces and functions

--- module-05/m05-start/define_generics_in_typescript.ts
+++ module-05/m05-start/define_generics_in_typescript.ts
@@ -8,9 +8,17 @@
 type ValidTypes = string | number;
 
 // aqui se le dice a T que los unicos tipos que puede aceptar son de tipo string y number, esto sirve para que cuando se ponga el argumento en la funcion, typescript detecte cualquier valor que no sea un string o un number, de esta manera mostrara el error de que el valor no satisface los tipos de el generic
-function identity<T extends ValidTypes, U> (value: T, message: U) : T {
-    let result: T = value + value;    // Error
-    console.log(message);
+function identity<T extends ValidTypes, U> (value: T, message: U) {
+    let result: ValidTypes = ''    // el resultado solo puede ser un numero o un string
+    let typeValue: string = typeof value // devuelve el tipo para usarlo en el console.log
+    //console.log(message);
+    //aqui typscript hara la prueba a ver si pasa la condicion, ya sea que sea un numero o un string, de esta manera se evita el error resaltado en value + value, es importante que cada uno tenga su condicional, para que se tomen de manera independiente
+    if (typeof value === 'number') {           // Is it a number?
+        result = value + value;                // OK
+    } else if (typeof value === 'string') {    // Is it a string?
+        result = value + value;                // OK
+    }
+    console.log(`The message is ${message} and the function returns a ${typeValue} value of ${result}`);
     return result
 }
 
@@ -31,3 +39,38 @@ let pets2 = { 1: "cats", 2: "dogs", 3: "parrots", 4: "fish"}
 
 console.log(getPets(pets1, "fish"));  // Returns 6
 console.log(getPets(pets2, "3"));     // Error
+
+// Exercise - Implement generics with interfaces and classes
+interface Identity<T, U> {
+    value: T;
+    message: U;
+}
+
+let returnNumber2: Identity<number, string> = {
+    value: 25,
+    message: 'Hello!'
+}
+let returnString2: Identity<string, number> = {
+    value: 'Hello!',
+    message: 25
+}
+function processIdentityTest(obj: Identity<number, string>): string {
+    let { value, message } = obj;
+    return `The string is: ${message}, and the number is ${value}`
+}
+
+//esta interfaz se puede aplicar a cualquier funcion que tenga una estrucura similar
+interface ProcessIdentity<T, U> {
+    (value: T, message: U): T
+}
+//creamos una funcion con la misma estructura que la interfaz ProcessIdentity
+function processIdentity<T, U> (value: T, message: U): T {
+    console.log(message);
+    return value
+}
+//definimos una variable que guardara la funcion processIdentity, y le asignamos la interfaz ProcessIdentity con los tipos number y string, entonces podremos llamar a la funcion que se guardo en la variable, esta debera tener los tipos que se definieron en la misma, ya que la interfaz es una funcion, se debe aplicar de esta forma
+let processor: ProcessIdentity<number, string> = processIdentity;//esto es una funcion, pero processIdentity(12, 'jjj') representa un valor de tipo number, por eso no se puede aplicar la interfaz de esta forma
+let returnNumber3 = processor(100, 'Hello!');
+let returnString3 = processor('Hello!', 100);
+// si no se le asigna la interfaz de forma explicita a la variable, typescript inferira los valores de esta
+let test = processIdentity(2, 'jj')
