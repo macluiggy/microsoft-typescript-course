68250a82885892242891604cf594e1741d970289
macluiggy <ferrinluiggy@gmail.com>
2021-11-22 09:30:27 -0500

    using modules in typescript

---
+++ access_external_libraries_from _typescript .ts/excercice02/Main.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+const result = dotenv.config({ path: './.env'})
+
+if (result.error) {
+    throw result.error;
+}
+console.log(result.parsed);  // Returns { DB_HOST: 'localhost', WEB_HOST: 'staging.adventure-works.com' }
+console.log(process.env.DB_HOST);
+console.log(process.env.WEB_HOST);
